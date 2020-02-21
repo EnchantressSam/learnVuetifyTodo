286f3bbe2c16dced9bb7882cf44fca6c3f2ae467
Tatyana Loginova <tatyanka8samara@mail.ru>
2020-02-21 14:22:11 +0400

    added sort

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,29 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
 
+    
+      <v-layout row class="mb-3">
+      <v-tooltip top>
+      <template v-slot:activator="{ on }">
+      <v-btn small text color="grey" @click="sortBy('title')" v-on="on">
+        <v-icon left small>mdi-folder</v-icon>
+        <span class="caption text-lowercase">By project name</span>
+      </v-btn>
+      </template>
+      <span>Sort projects by project name</span>
+       </v-tooltip>
+
+       <v-tooltip top>
+      <template v-slot:activator="{ on }">
+      <v-btn small text color="grey" @click="sortBy('person')" v-on="on">
+        <v-icon left small>mdi-account</v-icon>
+        <span class="caption text-lowercase">By person</span>
+      </v-btn>
+      </template>
+      <span>Sort projects by person</span>
+       </v-tooltip>
+       </v-layout>
+
       <v-card flat v-for="project in projects" :key="project.title">
       <v-layout row wrap :class="`pa-3 project ${project.status}`">
         <v-flex xs12 md6>
@@ -24,10 +47,9 @@
             <div>{{ project. due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-          <div class="caption grey--text">
-            Status
-            </div>
-            <div>{{ project.status }}</div>
+           <div align="right">
+             <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
+           </div>
           </v-flex>
       </v-layout>
       <v-divider></v-divider>
@@ -53,6 +75,11 @@ export default {
       ]
     }
   },
+  methods: {
+    sortBy(prop){
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  },
   }
 
 </script>
@@ -62,9 +89,21 @@ export default {
     border-left: 4px solid #3cd1c2;
   }
   .project.ongoing{
-    border-left: 4px solid orange;
+    border-left: 4px solid #ffaa2c;
   }
   .project.overdue{
-    border-left: 4px solid tomato;
+    border-left: 4px solid #f83e70;
   }
+  
+  .v-chip.complete{
+    background: #3cd1c2 !important;
+  }
+  .v-chip.ongoing{
+    background: #ffaa2c !important;
+  }
+  .v-chip.overdue{
+    background: #f83e70 !important;
+  }
+  
+  
 </style>
