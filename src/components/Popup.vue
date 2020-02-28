04060e357402a3893977d8c082e058c7df2033de
Tatyana Loginova <tatyanka8samara@mail.ru>
2020-02-28 15:16:29 +0400

    added validation rules

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,63 +11,18 @@
                 <h2>Add a New Project</h2>
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3">
-                    <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
-                    <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
-                    <!-- date pickers 
-                    <v-menu
-          v-model="menu2"
-          :close-on-content-click="false"
-          max-width="290"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              :value="computedDateFormattedDatefns"
-              clearable
-              label="Formatted with datefns"
-              readonly
-              v-on="on"
-              @click:clear="date = null"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="date"
-            @change="menu2 = false"
-          ></v-date-picker>
-        </v-menu>
-        
-
-        <script>
-  import moment from 'moment'
-  import format from 'date-fns/format'
-
-  export default {
-    data: () => ({
-      date: new Date().toISOString().substr(0, 10),
-      menu1: false,
-      menu2: false,
-    }),
-
-    computed: {
-      computedDateFormattedMomentjs () {
-        return this.date ? moment(this.date).format('dddd, MMMM Do YYYY') : ''
-      },
-      computedDateFormattedDatefns () {
-        return this.date ? format(this.date, 'dddd, MMMM Do YYYY') : ''
-      },
-    },
-  }
-</script>
-        
-        -->
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder" :rules="inputRules"></v-text-field>
+                    <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil" :rules="inputRules"></v-textarea>
+                    <!-- date pickers -->
                     <v-menu max-width="290">
                         <template v-slot:activator="{ on }">
-                            <v-text-field v-on="on" :value="formattedDate" label="Due date" prepend-icon="mdi-calendar-range" >
+                            <v-text-field :rules="inputRules" v-on="on" :value="formattedDate" label="Due date" prepend-icon="mdi-calendar-range" >
                             </v-text-field>
                             </template>
                             <v-date-picker v-model="due" ></v-date-picker>
                     </v-menu>
-
+                      <v-spacer></v-spacer>
                     <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
                 </v-form>
             </v-card-text>
@@ -76,8 +31,7 @@
     </v-dialog>
 </template>
 <script>
-//import format from 'date-fns/format'
-//import parseISO from 'date-fns/parseISO'
+
 import moment from 'moment'
 export default {
     data() {
@@ -85,12 +39,17 @@ export default {
            title: '',
            content: '',
           due: null,
+          inputRules: [
+            v => v.length >= 3 || 'Minimum length is 3 characters' 
+          ]
                    
         }
     },
     methods: {
         submit(){
-          //console.log(this.title, this.content)  
+           if (this.$refs.form.validate()){
+          //console.log(this.title, this.content) 
+          } 
         }
     },
     computed: {
